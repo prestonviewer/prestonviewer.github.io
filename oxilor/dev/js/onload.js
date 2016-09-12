d473205c4d313fa27d6c4072d1d4fc2fa02c7301
prestonviewer <prestonviewer@gmail.com>
2016-09-12 13:48:41 +0300

    Исправлено отображение операций элемента списка при наведении мышкой

--- oxilor/dev/js/onload.js
+++ oxilor/dev/js/onload.js
@@ -8,4 +8,23 @@ $(document).ready(function() {
       window.fullWidth = false;
     }
   });
+
+  $('.list-item').hover(function(e) {
+    var
+      textWidth = $(this).find('.list-item-text').width(),
+      textOffsetLeft = $(this).find('.list-item-text').get(0).offsetLeft,
+      opsWidth = $(this).find('.list-item-operations').width()
+    ;
+
+    if (textWidth + textOffsetLeft + opsWidth > $(this).width()) {
+      $(this).find('.list-item-operations').css({
+        position: 'absolute',
+        top: 0,
+        right: 0
+      });
+
+      // Patching only once per element
+      $(this).off('mouseover mouseout mouseenter mouseleave');
+    }
+  });
 });
